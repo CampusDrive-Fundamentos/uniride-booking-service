f65bd49f091588b280237286d4f32a0b1ed836b9
ErickLeo13 <u20231a500@upc.edu.pe>
2026-05-09 18:47:13 -0500

    Add files via upload

---
+++ src/main/java/com/uniride/uniridebookingservice/UnirideBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniride.uniridebookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirideBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnirideBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniride/uniridebookingservice/UnirideBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniride.uniridebookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnirideBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
